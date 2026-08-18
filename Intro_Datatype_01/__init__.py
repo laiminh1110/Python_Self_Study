3d73232563a5494765cfe73410aaad64a937a8d3
minhl <minhl@scb.com.vn>
2026-08-18 14:21:13 +0700

    commit create folder

---
+++ Intro_Datatype_01/__init__.py
@@ -0,0 +1,50 @@
+"""
+Python language programing
+Buổi 1 - Intro - Data Type (Kiểu dữ liệu) - Thầy dạy 4 kiểu dữ liệu (Number, Str, List, Tuple)
+
+- Trước khi tìm hiểu 1 ngôn ngữ lập trình nào đó cần phải biết ngôn ngữ đó hỗ trợ cho chung ta là các kiểu dữ liệu gì
+- Python (Int, float,str,list,tuple, dic, Set, Boolean, None ....)
+- Swift (Int, Double, String, Array, Dictionary, set... )
+- Tải sao phải dùng python -> Đã tạo ra 1 hệ sinh thái các thư viện, module, package hỗ trợ cho AI, Data Science, ML, Deep learning, GIS, Remote Sensing
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Buổi 2 -  Kiểu dữ liệu tiếp theo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
